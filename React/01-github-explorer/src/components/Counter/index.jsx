3566e3f1f6e8d2e78342e048ee3a5cad9f7235ba
lucianolimafer <lucianolimaf3r@gmail.com>
2021-03-25 15:14:33 -0300

    :book: Learn about components on state

---
+++ React/01-github-explorer/src/components/Counter/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  function decrement() {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
+
+  return (
+    <div>
+      <h2>{counter}</h2>
+      <button type="button" onClick={increment}>
+        Incremente
+      </button>
+      <button type="button" onClick={decrement}>
+        Decremente
+      </button>
+    </div>
+  )
+}
